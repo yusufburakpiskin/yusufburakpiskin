eb28730c235cde161e0109a1164e33f40d27f126
Bozo60 <yusufburakpiskin@gmail.com>
2020-06-30 02:13:21 +0300

    Update KullaniciController.php

--- app/Http/Controllers/KullaniciController.php
+++ app/Http/Controllers/KullaniciController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Http\Request;
+use Illuminate\Database\Query\Builder;
 use App\User;
 class KullaniciController extends Controller
 {
